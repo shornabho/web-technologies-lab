d3f384320df2b69e93d8949d97d08e6e039dcf47
Swarnava Ghosh <swarnava98@gmail.com>
2021-10-27 14:10:03 +0530

    Prettier Reformat, And Print Reformat

--- CAT2/js/domparser.js
+++ CAT2/js/domparser.js
@@ -2,12 +2,20 @@ const parser = new DOMParser();
 
 let studentsData;
 fetch('http://127.0.0.1:5500/Lab/CAT2/students.xml')
-    .then(response => response.text())
-    .then(response => {
-        studentsData = response;
-        // console.log(studentsData);
-        const xmlDoc = parser.parseFromString(studentsData, 'text/xml');
-        const student = xmlDoc.getElementsByTagName('STUDENT');
-        console.log(student);
-    });
+	.then((response) => response.text())
+	.then((response) => {
+		studentsData = response;
+		const xmlDoc = parser.parseFromString(studentsData, 'text/xml');
+		const students = Array.from(xmlDoc.getElementsByTagName('STUDENT'));
 
+		console.log(students);
+		students.forEach((student) => {
+			console.log(
+				`Name: ${
+					student.getElementsByTagName('STU-NAME')[0].textContent
+				}; College: ${
+					student.getElementsByTagName('STU-COLLEGE')[0].textContent
+				}`
+			);
+		});
+	});
--- CAT2/js/register.js
+++ CAT2/js/register.js
@@ -13,67 +13,71 @@ const collegeField = document.getElementById('college');
 const registrationForm = document.getElementById('registrationForm');
 
 const updateUIValidationState = (field, state) => {
-    if (state) {
-        field.classList.remove('is-invalid');
-        field.classList.add('is-valid');
-    }
-    else {
-        field.classList.remove('is-valid');
-        field.classList.add('is-invalid');
-    }
+	if (state) {
+		field.classList.remove('is-invalid');
+		field.classList.add('is-valid');
+	} else {
+		field.classList.remove('is-valid');
+		field.classList.add('is-invalid');
+	}
 };
 
 const validateName = (event) => {
-    const name = nameField.value;
-    const re = /[a-zA-Z]/;
-    const validName = re.test(String(name)) && name.length !== 0;
-    updateUIValidationState(nameField, validName);
+	const name = nameField.value;
+	const re = /[a-zA-Z]/;
+	const validName = re.test(String(name)) && name.length !== 0;
+	updateUIValidationState(nameField, validName);
 };
 
 const validateEmail = (event) => {
-    const email = emailField.value;
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    const validEmail = re.test(String(email)) && email.length !== 0;
-    updateUIValidationState(emailField, validEmail);
+	const email = emailField.value;
+	const re =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	const validEmail = re.test(String(email)) && email.length !== 0;
+	updateUIValidationState(emailField, validEmail);
 };
 
 const validateUsername = (event) => {
-    const username = usernameField.value;
-    const re = /^[A-Z]+[a-zA-Z0-9]/;
-    const validUsername = re.test(String(username)) && username.length !== 0;
-    updateUIValidationState(usernameField, validUsername);
+	const username = usernameField.value;
+	const re = /^[A-Z]+[a-zA-Z0-9]/;
+	const validUsername = re.test(String(username)) && username.length !== 0;
+	updateUIValidationState(usernameField, validUsername);
 };
 
 const validatePassword = (event) => {
-    const password = passwordField.value;
-    const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    const reEmbeddedCheck = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+	const password = passwordField.value;
+	const re =
+		/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+	const reEmbeddedCheck =
+		/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
 
-    const validPassword = re.test(String(password)) && reEmbeddedCheck.test(String(password.slice(1, password.length - 1)));
+	const validPassword =
+		re.test(String(password)) &&
+		reEmbeddedCheck.test(String(password.slice(1, password.length - 1)));
 
-    updateUIValidationState(passwordField, validPassword);
+	updateUIValidationState(passwordField, validPassword);
 };
 
 const validateConfirmPassword = (event) => {
-    const confirmPassword = confirmPasswordField.value;
-    const validConfirmPassword = passwordField.value == confirmPassword;
+	const confirmPassword = confirmPasswordField.value;
+	const validConfirmPassword = passwordField.value == confirmPassword;
 
-    updateUIValidationState(confirmPasswordField, validConfirmPassword);
+	updateUIValidationState(confirmPasswordField, validConfirmPassword);
 };
 
 const validateAge = (event) => {
-    const age = ageField.value;
-    const validAge = age > 0;
-    updateUIValidationState(ageField, validAge);
+	const age = ageField.value;
+	const validAge = age > 0;
+	updateUIValidationState(ageField, validAge);
 };
 
 const validatePhoneNumber = (event) => {
-    const phoneNumber = phoneNumberField.value;
-    const re = /[0-9]{10}/;
-    const validPhoneNumber = re.test(String(phoneNumber)) && phoneNumber.length == 10;
-    updateUIValidationState(phoneNumberField, validPhoneNumber);
+	const phoneNumber = phoneNumberField.value;
+	const re = /[0-9]{10}/;
+	const validPhoneNumber =
+		re.test(String(phoneNumber)) && phoneNumber.length == 10;
+	updateUIValidationState(phoneNumberField, validPhoneNumber);
 };
-
 
 nameField.addEventListener('keyup', validateName);
 emailField.addEventListener('keyup', validateEmail);
@@ -83,11 +87,11 @@ confirmPasswordField.addEventListener('keyup', validateConfirmPassword);
 ageField.addEventListener('keyup', validateAge);
 phoneNumberField.addEventListener('keyup', validatePhoneNumber);
 
-
 registrationForm.addEventListener('submit', (event) => {
-    localStorage.setItem('username', usernameField.value);
-    localStorage.setItem('phoneNumber', phoneNumberField.value);
+	localStorage.setItem('username', usernameField.value);
+	localStorage.setItem('phoneNumber', phoneNumberField.value);
 
-    var xmlDoc = document.implementation.createDocument(null, "students");
+	document.cookie = `username:${usernameField.value}; phoneNumber:${phoneNumberField.value}`;
 
+	var xmlDoc = document.implementation.createDocument(null, 'students');
 });
